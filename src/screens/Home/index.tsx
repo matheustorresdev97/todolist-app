abf33a2e055d85c52915e4685bbef390f2b94153
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-19 10:24:14 -0300

    ":lipstick: feat: Style HomeScreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { styles } from "./styles"
+import { Header } from "../../components/Header";
 
 export function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Header />
     </View>
   );
 }

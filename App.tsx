63a59858761b7134dcb90e2ae09ee2db0cd4580c
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-03-19 10:19:04 -0300

    :sparkles: feat: Criando HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { HomeScreen } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {fontsLoaded ? <HomeScreen /> : <Loading />}
+      <StatusBar style="auto" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import { ActivityIndicator, View } from 'react-native'
 import { theme } from '../../theme'
 import { styles } from './styles'
 
-const Loading = () => {
+export const Loading = () => {
     return (
         <View style={styles.container}>
             <ActivityIndicator size={50} color={theme.colors.brand.blue} />
@@ -10,4 +10,3 @@ const Loading = () => {
     )
 }
 
-export default Loading
